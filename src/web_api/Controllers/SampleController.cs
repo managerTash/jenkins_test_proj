b6f8120627e1b17fe131d448350227aa354c2f22
Emmanuel Owusu <eowusu@acsghana.com>
2019-07-24 13:59:26 +0000

    web api sample controller added

---
+++ src/web_api/Controllers/SampleController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace web_api.Controllers
+{
+    [Produces("application/json")]
+    [Route("[controller]")]
+    public class SampleController : ControllerBase
+    {
+
+        [HttpGet("[action]")]
+        public (bool, string) GetData()
+        {
+            return (true, "This is a test response");
+        }
+    }
+}
